b7c8f58fe33a0b97eb26e0a4413ee8e9143c1ed8
mhbughdadi <mohammed_hassan2525@gmail.com>
2025-06-29 14:19:15 +0400

    chore: moving the product to products-ms repository

--- product/src/main/java/com/apogee/product/dtos/output/ErrorMessage.java
+++
@@ -1,7 +0,0 @@
-package com.apogee.product.dtos.output;
-
-import org.springframework.http.HttpStatus;
-
-public record ErrorMessage(HttpStatus httpStatus, String errorCode, String errorDescriptionEn, String errorDescriptionAr
-) {
-}
--- product/src/main/java/com/apogee/product/repositories/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apogee.product.repositories;
-
-import com.apogee.product.entities.ImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
-}
